80b1bd2fd512d6a6b39e3065569784bfba0fe158
Dominic <Dominic.heal@europe.com>
2016-05-10 20:00:55 +0100

    Smoother animation and text

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,11 +3,17 @@ package sample;
 import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.canvas.Canvas;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextBoundsType;
 import javafx.util.Duration;
 
 import java.net.URL;
@@ -20,27 +26,53 @@ public class Controller implements Initializable {
     private Rectangle rectangle;
     private Text text;
     private int count = 0;
+    private boolean isRunning;
 
 
-    public Rectangle createRetangle(){
+    public Rectangle createRectangle(){
         rectangle = new Rectangle(10,200,100, 30);
         rectangle.setArcHeight(15);
         rectangle.setArcWidth(15);
         rectangle.setFill(Color.ORANGE);
-        rectangle.setTranslateX(100);
-        rectangle.setTranslateY(100);
         rectangle.setSmooth(true);
-        rectangle.setOnMousePressed(t -> preload());
+        rectangle.setOnMousePressed(t -> {
+            if (!isTaskRunning()){
+                preload();
+            }
+        });
+        rectangle.setOnMouseEntered(event -> {
+            if (!isTaskRunning()) {
+                rectangle.setOpacity(0.7);
+                rectangle.setCursor(Cursor.HAND);
+            }
+        });
+        rectangle.setOnMouseExited(event -> {
+            if (!isTaskRunning()) {
+                rectangle.setOpacity(1);
+                rectangle.setCursor(Cursor.DEFAULT);
+            }
+        });
         return rectangle;
     }
 
     private void preload() {
         try {
+            setTaskRunning(true);
+            rectangle.setCursor(Cursor.WAIT);
             text.setVisible(false);
             runAnimation();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private void setTaskRunning(boolean running) {
+       isRunning = running;
+
+    }
+
+    private boolean isTaskRunning(){
+        return isRunning;
     }
 
     public void runAnimation() throws InterruptedException {
@@ -53,35 +85,30 @@ public class Controller implements Initializable {
         Timeline timeline = new Timeline();
         timeline.getKeyFrames().add(keyFrame);
 
-        TranslateTransition translateTransition = new TranslateTransition(Duration.millis(500), rectangle);
-        translateTransition.setFromX(100);
-        translateTransition.setToX(125);
-
         FillTransition fltr = new FillTransition(Duration.millis(500), rectangle, Color.ORANGE, Color.TRANSPARENT);
         StrokeTransition stroke = new StrokeTransition(Duration.millis(500), rectangle, Color.TRANSPARENT, Color.DARKGRAY);
 
-
         ParallelTransition ptr = new ParallelTransition();
-        ptr.getChildren().addAll(fltr, translateTransition, timeline, stroke);
+        ptr.getChildren().addAll(fltr, timeline, stroke);
 
         RotateTransition rotation = new RotateTransition(Duration.millis(2000), rectangle);
         rotation.setFromAngle(0);
         rotation.setToAngle(360);
 
-        StrokeTransition strokeSucess = new StrokeTransition(Duration.millis(500), rectangle, Color.DARKGRAY,  Color.ORANGE);
+        StrokeTransition strokeSuccess = new StrokeTransition(Duration.millis(500), rectangle, Color.DARKGRAY,  Color.ORANGE);
 
-        SequentialTransition seqential = new SequentialTransition();
-        seqential.getChildren().addAll(ptr, rotation, strokeSucess );
+        SequentialTransition sequential = new SequentialTransition();
+        sequential.getChildren().addAll(ptr, rotation, strokeSuccess );
 
-        seqential.setOnFinished(event -> {
+        sequential.setOnFinished(event -> {
             count++;
             if (count % 2 == 0){
-                runSucessfulAnimation();
+                runSuccessfullyAnimation();
             } else {
                 runFailedAnimation();
             }
         });
-        seqential.play();
+        sequential.play();
     }
 
     private void runFailedAnimation() {
@@ -94,26 +121,21 @@ public class Controller implements Initializable {
         Timeline timeline = new Timeline();
         timeline.getKeyFrames().add(keyFrame);
 
-        TranslateTransition translateTransition = new TranslateTransition(Duration.millis(500), rectangle);
-        translateTransition.setFromX(125);
-        translateTransition.setToX(100);
-
         FillTransition fltr = new FillTransition(Duration.millis(500), rectangle,  Color.LIGHTGRAY, Color.RED.brighter());
-        StrokeTransition strokeSucess = new StrokeTransition(Duration.millis(500), rectangle,  Color.ORANGE, Color.RED);
+        StrokeTransition strokeSuccess = new StrokeTransition(Duration.millis(500), rectangle,  Color.ORANGE, Color.RED);
 
         ParallelTransition ptr = new ParallelTransition();
-        ptr.getChildren().addAll(fltr, translateTransition, timeline);
+        ptr.getChildren().addAll(fltr, timeline);
 
         SequentialTransition seqential = new SequentialTransition();
         seqential.setOnFinished(event -> {
-            text.setText("Failed");
-            text.setVisible(true);
+            onComplete("Failed");
         });
-        seqential.getChildren().addAll(strokeSucess, ptr);
+        seqential.getChildren().addAll(strokeSuccess, ptr);
         seqential.play();
     }
 
-    private void runSucessfulAnimation() {
+    private void runSuccessfullyAnimation() {
         KeyValue kv = new KeyValue(rectangle.widthProperty(), 100);
         KeyValue kv2 = new KeyValue(rectangle.xProperty(), 10);
         KeyValue kv3 = new KeyValue(rectangle.arcHeightProperty(), 15);
@@ -123,45 +145,62 @@ public class Controller implements Initializable {
         Timeline timeline = new Timeline();
         timeline.getKeyFrames().add(keyFrame);
 
-        TranslateTransition translateTransition =
-                new TranslateTransition(Duration.millis(500), rectangle);
-        translateTransition.setFromX(125);
-        translateTransition.setToX(100);
-
         FillTransition fltr = new FillTransition(Duration.millis(500), rectangle, Color.LIGHTGRAY, Color.rgb(46, 204, 113));
 
         ParallelTransition ptr = new ParallelTransition();
-        ptr.getChildren().addAll(fltr, translateTransition, timeline);
+        ptr.getChildren().addAll(fltr, timeline);
 
-        StrokeTransition strokeSucess = new StrokeTransition(Duration.millis(500), rectangle, Color.ORANGE, Color.rgb(46, 204, 113));
+        StrokeTransition strokeSuccess = new StrokeTransition(Duration.millis(500), rectangle, Color.ORANGE, Color.rgb(46, 204, 113));
 
-        SequentialTransition seqential = new SequentialTransition();
-        seqential.getChildren().addAll(strokeSucess, ptr);
-        seqential.setOnFinished(event -> {
-            text.setText("Success!");
-            text.setVisible(true);
+        SequentialTransition sequential = new SequentialTransition();
+        sequential.getChildren().addAll(strokeSuccess, ptr);
+        sequential.setOnFinished(event -> {
+            onComplete("Success");
         });
-        seqential.play();
+        sequential.play();
+
+    }
+
+    private void onComplete(String textMessage) {
+        text.setText(textMessage);
+        text.setVisible(true);
+        rectangle.setCursor(Cursor.HAND);
+        setTaskRunning(false);
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        flowPane.getChildren().add(createCanvas());
-        flowPane.getChildren().addAll(createRetangle(), createText());
+        StackPane stackPane = new StackPane();
+        stackPane.setPadding(new Insets(20));
+        stackPane.getChildren().addAll(createRectangle(), createText());
+        flowPane.getChildren().add(stackPane);
+        flowPane.setAlignment(Pos.CENTER);
+
     }
 
-    private Canvas createCanvas() {
-        return new Canvas();
-    }
 
     private Text createText() {
         text = new Text();
         text.setText("Connect");
-        text.setTranslateX(27);
-        text.setTranslateY(100);
+        text.setFont(Font.font("Open Sans"));
         text.setFill(Color.WHITE);
-        text.setOnMousePressed(t -> preload());
+        text.setSmooth(true);
+        text.setBoundsType(TextBoundsType.VISUAL);
+        text.setOnMousePressed(t -> {
+            if (!isTaskRunning()){
+                preload();
+            }
+        });
+        text.setOnMouseEntered(event -> {
+            rectangle.setOpacity(0.7);
+            text.setCursor(Cursor.HAND);
+        });
+        text.setOnMouseExited(event -> {
+            rectangle.setOpacity(1);
+            text.setCursor(Cursor.DEFAULT);
+        });
+        text.setTextAlignment(TextAlignment.CENTER);
         return text;
     }
 }
